aaee1ea3b6aace59afeff6c21a47eb819f6a1bba
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-10-02 21:56:27 +0800

    Again practice function

--- function.js
+++ function.js
@@ -5,4 +5,22 @@ var func = function sayHello() {
     console.log('Do you like lolipop?');
 }
 
-func();
+// func();
+
+function doubleIt (num) {
+    var result = num * 2;
+    return result;
+}
+
+var firstNum = doubleIt(4);
+var secondNum = doubleIt(10);
+
+
+
+function add(num1 = 5, num2 = 10) {         //set default value
+    return num1 + num2;
+}
+
+var total = add(20, 10);
+
+console.log(total);
