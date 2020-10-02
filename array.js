6cb64fe1b78226f103192424dcd0cd304d01b783
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-10-02 19:02:23 +0800

    Reacp array

--- array.js
+++ array.js
@@ -1,7 +1,19 @@
 var friendAges = [12, 44, 21, 17, 19];
+
 friendAges[2] = 33;
+
 var sabbirAge = friendAges[2];
 
 var position = friendAges.indexOf(22);
 
-console.log(position);
+// console.log(position);
+
+
+var friendsName = ['Abir', 'Kabir', 'Nasir', 'Babir', 'Sanir'];
+
+var babirPosition = friendsName.indexOf('Babir');
+var dabirPosition = friendsName.indexOf('Dabir');
+
+friendsName[2] = 'Basir';
+
+console.log(friendsName);
