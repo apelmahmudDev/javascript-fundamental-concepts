e8fc99968aa80356016d755b2ba544f14f77ed2e
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-10-02 21:35:55 +0800

    Again practice function at javascript fundamental concepts

---
+++ function.js
@@ -0,0 +1,8 @@
+
+var func = function sayHello() {
+    console.log('I love you baby');
+    console.log('Where are you?');
+    console.log('Do you like lolipop?');
+}
+
+func();
---
+++ switch.js
@@ -0,0 +1,12 @@
+var num = 5;
+
+switch (num) {
+    case 10:
+        console.log('I am 10');
+        break;
+    case 5:
+        console.log('I am 5');
+        break;
+    default:
+        console.log("I don't know, Who are you");
+}
